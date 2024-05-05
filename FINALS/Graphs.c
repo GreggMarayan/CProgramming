7187aa9cb01d48d2014490b7ccdfe2fcaf4b97d3
Gregg <gregg.marayan@gmail.com>
2024-05-05 12:55:46 +0800

    Done Kruskals!

--- FINALS/Graphs.c
+++ FINALS/Graphs.c
@@ -16,7 +16,7 @@ typedef struct {
 typedef int SET[MAX];
 
 int prims(MATRIX);
-int kruskal(MATRIX);
+GRAPH kruskal(MATRIX);
 
 int main(void) {
     MATRIX MAIN = { {Z, 1,  5,  4,  6,  5},
@@ -26,9 +26,10 @@ int main(void) {
                     {6, Z,  Z,  1,  Z,  3},
                     {5, 6,  Z,  Z,  3,  Z} };
 
-    prims(MAIN);
+    GRAPH ror = kruskal(MAIN);
+    printf("%d", ror.TotalCost);
 
-    printf("%d",prims(MAIN));
+    // printf("%d",prims(MAIN));
 
     return 0;
 }
@@ -69,55 +70,51 @@ int prims(MATRIX graph) {
     return totalCost;
 }
 
+GRAPH kruskal(MATRIX main){
 
-int kruskal(MATRIX graph){
-    int retval = 0;
-    int ndx,ind, min_w, min_x, min_y;
-    int edgeCTR = 0;
-    MATRIX MST;
-    for(ndx = 0; ndx < MAX; ndx++){
-        for(ind = 0; ind < MAX; ind++){
-            MST[ndx][ind] = Z;
-        }
+    int small, smallndy, smallndx, edgeCTR = 0;
+    GRAPH retval = {.TotalCost = 0};
+
+    int SET[MAX];
+    for(int ndx = 0; ndx < MAX; ndx++){
+        SET[ndx] = -1;
     }
 
-    SET parent;
-    for(ndx = 0; ndx < MAX; ndx++){
-        parent[ndx] = ndx;
-    }
+    while(edgeCTR < MAX-1){
+        small = Z;
 
-    while (edgeCTR < MAX - 1)   
-    {
-
-        //searching for the smallest weighted edge.
-        min_w = Z;
-        for(ndx = 0; ndx < MAX; ndx++){
-            for(ind = ndx + 1; ind < MAX; ind++){
-                if(graph[ndx][ind] < min_w){
-                    min_w = graph[ndx][ind];
-                    min_x = ndx;
-                    min_y = ind;
+        //finds smallest edge amoang the graph
+        for(int ndx = 0; ndx < MAX; ndx++){
+            for(int ndy = ndx; ndy < MAX; ndy++){
+                if(retval.M[ndx][ndy] != Z && main[ndx][ndy] < small){
+                    small = main[ndx][ndy];
+                    smallndx = ndx;
+                    smallndy = ndy;
                 }
             }
         }
 
-        //checking if the found smallest edge would create a cycle
-        int x,y;
-        for(x = min_x, y = min_y; (parent[x] != x) || (parent[y] != y); x = parent[x], y = parent[y]){}
+        //checks if the 2 vertexes are different components
+        int tempx = smallndx, tempy = smallndy;
+        for(;SET[tempx] > 0;tempx = SET[tempx]){}
+        for(;SET[tempy] > 0;tempy = SET[tempy]){}
 
 
-        //if it doesnt create a cycle then add the two segments together
-        if(x != y){
-            retval += min_w;
-            MST[x][y] = min_w;
+        //Adds the info to the graph if it is not in the same component
+        if(tempx != tempy){
+            retval.TotalCost += small;
+            retval.M[smallndx][smallndy] = retval.M[smallndy][smallndx] = small;
             edgeCTR++;
-            // Union operation in disjoint set
-            parent[y] = x;
+
+        //removes the vertex from main graph;
+            main[smallndx][smallndy] = Z;
+
+        //Updates the Disjoint Set SET[]
+            SET[smallndx] += SET[smallndy];
+            SET[smallndy] = smallndx;
         }
-
-        // Mark this edge as visited
-        graph[min_x][min_y] = graph[min_y][min_x] = Z;
     }
-
     return retval;
 }
+
+
