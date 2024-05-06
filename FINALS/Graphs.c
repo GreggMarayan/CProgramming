0004d0bed3d4f66f4dfc1687c23a7128b11f4981
Gregg <gregg.marayan@gmail.com>
2024-05-06 11:34:35 +0800

    fixed prims algorithim

--- FINALS/Graphs.c
+++ FINALS/Graphs.c
@@ -15,7 +15,10 @@ typedef struct {
 
 typedef int SET[MAX];
 
-int prims(MATRIX);
+
+
+
+GRAPH prims(MATRIX);
 GRAPH kruskal(MATRIX);
 
 int main(void) {
@@ -27,48 +30,88 @@ int main(void) {
                     {5, 6,  Z,  Z,  3,  Z} };
 
     GRAPH ror = kruskal(MAIN);
-    printf("%d", ror.TotalCost);
+    GRAPH rar = prims(MAIN);
+    printf("%d = %d", ror.TotalCost,rar.TotalCost);
 
     // printf("%d",prims(MAIN));
 
     return 0;
 }
 
-int prims(MATRIX graph) {
-    int totalCost = 0;
-    SET visited = { 0 }; // Initialize visited array
+// int prims(MATRIX graph) {
+//     int totalCost = 0;
+//     SET visited = { 0 }; // Initialize visited array
 
-    // Start from vertex 0
-    visited[A] = 1;
+//     // Start from vertex 0
+//     visited[A] = 1;
 
-    // Number of edges to be added to the MST
-    int edgeCount = 0;
+//     // Number of edges to be added to the MST
+//     int edgeCount = 0;
 
-    // Repeat until all vertices are visited
-    while (edgeCount < MAX - 1) {
-        int minWeight = Z;
-        int minVertex = -1;
+//     // Repeat until all vertices are visited
+//     while (edgeCount < MAX - 1) {
+//         int minWeight = Z;
+//         int minVertex = -1;
 
-        // Find the minimum weight edge from a vertex in the MST to a vertex outside the MST
-        for (int i = 0; i < MAX; ++i) {
-            if (visited[i]) {
-                for (int j = 0; j < MAX; ++j) {
-                    if (!visited[j] && graph[i][j] < minWeight) {
-                        minWeight = graph[i][j];
-                        minVertex = j;
+//         // Find the minimum weight edge from a vertex in the MST to a vertex outside the MST
+//         for (int i = 0; i < MAX; ++i) {
+//             if (visited[i]) {
+//                 for (int j = 0; j < MAX; ++j) {
+//                     if (!visited[j] && graph[i][j] < minWeight) {
+//                         minWeight = graph[i][j];
+//                         minVertex = j;
+//                     }
+//                 }
+//             }
+//         }
+
+//         // Add the minimum weight edge to the MST
+//         totalCost += minWeight;
+//         visited[minVertex] = 1;
+//         edgeCount++;
+//     }
+
+//     return totalCost;
+// }
+
+GRAPH prims(MATRIX main){
+    GRAPH retval = {.TotalCost = 0};
+    int SET[MAX] = {1,0,0,0,0,0};
+    int edgeCTR = 0, head, end;
+
+    while(edgeCTR < MAX-1){
+        int small = Z, u  = 0, v = 0;
+        for(head = 0; head < MAX; head++){
+            if(SET[head]){
+                for(end = 0; end < MAX; ++end){
+                    if(SET[end] != 1 && main[head][end] < small){
+                        small = main[head][end];
+                        u = head;
+                        v = end;
                     }
                 }
             }
         }
+        SET[v] = 1;
 
-        // Add the minimum weight edge to the MST
-        totalCost += minWeight;
-        visited[minVertex] = 1;
-        edgeCount++;
+        if(small != Z){
+            retval.M[u][v] = small;
+            retval.TotalCost += small;
+        }
+     edgeCTR++;
     }
-
-    return totalCost;
+    return retval;
 }
+
+
+
+
+
+
+
+
+
+
 
 GRAPH kruskal(MATRIX main){
 
@@ -86,7 +129,7 @@ GRAPH kruskal(MATRIX main){
         //finds smallest edge amoang the graph
         for(int ndx = 0; ndx < MAX; ndx++){
             for(int ndy = ndx; ndy < MAX; ndy++){
-                if(retval.M[ndx][ndy] != Z && main[ndx][ndy] < small){
+                if(retval.M[ndx][ndy] != main[ndx][ndy] && main[ndx][ndy] < small){
                     small = main[ndx][ndy];
                     smallndx = ndx;
                     smallndy = ndy;
@@ -107,7 +150,7 @@ GRAPH kruskal(MATRIX main){
             edgeCTR++;
 
         //removes the vertex from main graph;
-            main[smallndx][smallndy] = Z;
+            // main[smallndx][smallndy] = Z;
 
         //Updates the Disjoint Set SET[]
             SET[smallndx] += SET[smallndy];
